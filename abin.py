7189d6085a53ed376935c540192c0ce5b437a355
abin-joshy <abinkuriakosejoshy2028@cs.sjcetpalai.ac.in>
2024-10-04 16:07:54 +0530

    Add files via upload

---
+++ abin.py
@@ -0,0 +1,31 @@
+'''print("hello world")
+if 5>2:
+    print("five is greater than two")
+    print("fve is greater than two")
+y='5'
+print(y,y)
+x=str(3)
+y=1j
+z=float(3)
+print(x)
+print(y,z)
+print(type(y))
+
+x,y='orange','pink'
+print(x)
+print(y)
+x="a"
+y="b"
+z="in"
+print(x+y+z)
+
+user=input("enter username:")
+print("usename is:" + user)
+
+a="hello"
+b="world"
+c=a+" "+b
+print(c)
+'''
+a="hello world"
+print(a[0:5])
