dd4032afab266ca07a21afdfab43d55dfde9e412
abin-joshy <abinkuriakosejoshy2028@cs.sjcetpalai.ac.in>
2024-10-18 16:20:22 +0530

    Add files via upload

---
+++ mathmodule.py
@@ -0,0 +1,11 @@
+'''author=abin joshy
+date=18-10-2024
+description=a Python program that demonstrates the usage of ar
+ithmetic, comparison, and logical operators.
+Perform a few operations and print the results.'''
+
+import math
+num=int(input("enter a number:"))
+print("square root of",num,"=",math.sqrt(num))
+print("Factorial of",num,"=",math.factorial(num))
+print(num,"raised to power 2=",math.pow(num,2))
