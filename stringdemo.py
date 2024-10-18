d70cd828de536abe08827ea62b93a054aff0c283
abin-joshy <abinkuriakosejoshy2028@cs.sjcetpalai.ac.in>
2024-10-18 15:09:30 +0530

    Add files via upload

---
+++ stringdemo.py
@@ -0,0 +1,10 @@
+"""
+author-abin joshy
+date:18-10-2024
+description:program twhich shows how concate different strings
+"""
+str1="Hello, World!"
+str2=str1[7:12]
+str3="everyone!"
+print(str2)
+print(str2+" "+str3)
