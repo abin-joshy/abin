7fb414e499dfff27fed20d67cb32a2a4e470f496
abin-joshy <abinkuriakosejoshy2028@cs.sjcetpalai.ac.in>
2024-10-18 15:57:02 +0530

    Add files via upload

---
+++ arithemtic_operators,py.py
@@ -0,0 +1,15 @@
+'''
+author:abin joshy
+date-18-10-2024
+description:a Python program that demonstrates the usage of
+arithmetic, comparison, and logical operators.
+Perform a few operations and print the results.
+'''
+num1=int(input("enter number1:"))
+num2=int(input("enter number2:"))
+print("sum:",num1+num2,end=",")
+print("divison:",num1/num2)
+print("is num1 greater than num2? ",num1>num2)
+print("Are num1 is equal to num2? ",num1==num2)
+print("logical and:",num1>0 and num2>0)
+print("logical or:",num2>0 or num2<9)
